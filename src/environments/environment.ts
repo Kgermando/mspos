0423206c374c8d41a66d43a31987612142aec285
Germain kataku <katakugermain@gmail.com>
2024-10-06 21:45:30 +0100

    change url api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true, 
-    apiUrl: 'https://mspos-api-dvp8.onrender.com/api'
+    apiUrl: 'https://mspos-api.onrender.com/api' //'https://mspos-api-dvp8.onrender.com/api'
 };
